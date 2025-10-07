47510f160794d6db452705f10a549a3227ede422
NetHackCorp <nethack.github495237@gmail.com>
2025-10-07 10:38:01 +0200

    feat: add column parameter to ProtoError structure and related function for enhanced error reporting

--- include/protohack/error.h
+++ include/protohack/error.h
@@ -12,10 +12,12 @@ typedef struct {
     bool ok;
     char message[256];
     size_t line;
+    size_t column;
 } ProtoError;
 
 void protoerror_reset(ProtoError *error);
 void protoerror_set(ProtoError *error, size_t line, const char *format, ...);
+void protoerror_set_with_column(ProtoError *error, size_t line, size_t column, const char *format, ...);
 
 #ifdef __cplusplus
 }
