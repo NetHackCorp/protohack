ccec80806f44fcc0407fc1f22870fd99a03e4e03
NetHackCorp <nethack.github495237@gmail.com>
2025-10-07 10:41:00 +0200

    feat: include JIT profiler functions in ProtoVM structure

--- include/protohack/vm.h
+++ include/protohack/vm.h
@@ -10,6 +10,7 @@
 #include "protohack/chunk.h"
 #include "protohack/function.h"
 #include "protohack/error.h"
+#include "protohack/jit.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -35,6 +36,10 @@ typedef struct ProtoVM {
     ProtoValue last_print_value;
 
     uint32_t rand_state;
+
+#if PROTOHACK_ENABLE_JIT
+    ProtoJITProfiler profiler;
+#endif
 } ProtoVM;
 
 void protovm_init(ProtoVM *vm);
@@ -42,6 +47,11 @@ void protovm_reset(ProtoVM *vm);
 bool protovm_run(ProtoVM *vm, const ProtoChunk *chunk, ProtoError *error);
 const ProtoValue *protovm_last_print(const ProtoVM *vm);
 void protovm_register_stdlib(ProtoVM *vm);
+
+#if PROTOHACK_ENABLE_JIT
+const ProtoJITProfiler *protovm_profiler(const ProtoVM *vm);
+void protovm_profiler_reset(ProtoVM *vm);
+#endif
 
 #ifdef __cplusplus
 }
