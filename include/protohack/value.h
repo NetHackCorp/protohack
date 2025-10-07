7225de96ca8ded17c78f72a5cd9ef76e9968bc7d
NetHackCorp <nethack.github495237@gmail.com>
2025-10-07 10:40:53 +0200

    feat: extend ProtoValue to include class, instance, and bound method types

--- include/protohack/value.h
+++ include/protohack/value.h
@@ -6,6 +6,7 @@
 
 #include "protohack/types.h"
 #include "protohack/typed_memory.h"
+#include "protohack/object.h"
 
 struct ProtoFunction;
 typedef struct ProtoFunction ProtoFunction;
@@ -20,16 +21,22 @@ typedef enum {
     PROTO_VAL_BOOL,
     PROTO_VAL_STRING,
     PROTO_VAL_FUNCTION,
+    PROTO_VAL_CLASS,
+    PROTO_VAL_INSTANCE,
+    PROTO_VAL_BOUND_METHOD,
     PROTO_VAL_MEMORY
 } ProtoValueType;
 
-typedef struct {
+typedef struct ProtoValue {
     ProtoValueType type;
     union {
         double number;
         bool boolean;
         char *string;
-    ProtoFunction *function;
+        ProtoFunction *function;
+        ProtoClass *klass;
+        ProtoInstance *instance;
+        ProtoBoundMethod *bound_method;
         ProtoTypedMemory memory;
     } as;
 } ProtoValue;
@@ -39,6 +46,9 @@ ProtoValue proto_value_number(double value);
 ProtoValue proto_value_bool(bool value);
 ProtoValue proto_value_string(const char *data, size_t length);
 ProtoValue proto_value_function(ProtoFunction *function);
+ProtoValue proto_value_class(ProtoClass *klass);
+ProtoValue proto_value_instance(ProtoInstance *instance);
+ProtoValue proto_value_bound_method(ProtoBoundMethod *bound);
 ProtoValue proto_value_memory(ProtoTypedMemory memory);
 ProtoValue proto_value_copy(const ProtoValue *value);
 void proto_value_free(ProtoValue *value);
