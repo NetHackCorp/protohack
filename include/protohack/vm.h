68891d74c520820032fa0be96e5738ddb4226987
NetHackCorp <nethack.github495237@gmail.com>
2025-10-11 10:58:36 +0200

    feat: add ProtoSpecializationEntry and ProtoSpecializationTable structures to support function specialization

--- include/protohack/vm.h
+++ include/protohack/vm.h
@@ -20,11 +20,26 @@ typedef struct ProtoCallFrame {
     const ProtoFunction *function;
     size_t ip;
     ProtoValue *slots;
+    ProtoTypeBindingSet bindings;
 } ProtoCallFrame;
+
+typedef struct ProtoSpecializationEntry {
+    const ProtoFunction *template_function;
+    ProtoTypeBindingSet bindings;
+    ProtoFunction *specialization;
+    uint64_t fingerprint;
+    bool owned;
+} ProtoSpecializationEntry;
+
+typedef struct ProtoSpecializationTable {
+    ProtoSpecializationEntry entries[PROTOHACK_MAX_SPECIALIZATIONS];
+    size_t count;
+} ProtoSpecializationTable;
 
 typedef struct ProtoVM {
     ProtoValue stack[PROTOHACK_STACK_MAX];
     ProtoValue *stack_top;
+    uint32_t stack_generation[PROTOHACK_STACK_MAX];
 
     ProtoCallFrame frames[PROTOHACK_MAX_CALL_STACK];
     int frame_count;
@@ -37,6 +52,8 @@ typedef struct ProtoVM {
 
     uint32_t rand_state;
 
+    ProtoSpecializationTable specializations;
+
 #if PROTOHACK_ENABLE_JIT
     ProtoJITProfiler profiler;
 #endif
@@ -47,6 +64,9 @@ void protovm_reset(ProtoVM *vm);
 bool protovm_run(ProtoVM *vm, const ProtoChunk *chunk, ProtoError *error);
 const ProtoValue *protovm_last_print(const ProtoVM *vm);
 void protovm_register_stdlib(ProtoVM *vm);
+ProtoFunction *protovm_find_specialization(const ProtoVM *vm, const ProtoFunction *template_function, const ProtoTypeBindingSet *bindings);
+bool protovm_register_specialization(ProtoVM *vm, const ProtoFunction *template_function, const ProtoTypeBindingSet *bindings, ProtoFunction *specialization, bool take_ownership);
+void protovm_clear_specializations(ProtoVM *vm, bool free_specializations);
 
 #if PROTOHACK_ENABLE_JIT
 const ProtoJITProfiler *protovm_profiler(const ProtoVM *vm);
