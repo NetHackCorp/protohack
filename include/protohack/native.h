0935d8161696ac40fe82f6622a97540fc2687dc0
NetHackCorp <nethack.github495237@gmail.com>
2025-10-11 10:56:53 +0200

    feat: add ProtoNativeSignature structure and update ProtoNativeEntry with signature field

--- include/protohack/native.h
+++ include/protohack/native.h
@@ -8,6 +8,7 @@
 #include "protohack/config.h"
 #include "protohack/error.h"
 #include "protohack/value.h"
+#include "protohack/binding.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,10 +20,32 @@ typedef struct ProtoVM ProtoVM;
 typedef bool (*ProtoNativeFn)(ProtoVM *vm, const ProtoValue *args, uint8_t arg_count, ProtoValue *result, ProtoError *error);
 
 typedef struct {
+    ProtoTypeTag return_type;
+    uint8_t param_count;
+    ProtoTypeTag param_types[PROTOHACK_MAX_NATIVE_ARGS];
+    ProtoTypeBindingSet binding_contract;
+} ProtoNativeSignature;
+
+#define PROTO_NATIVE_SIGNATURE_SIMPLE(RETURN_TAG, PARAM_COUNT) \
+    ((ProtoNativeSignature){ \
+        .return_type = (RETURN_TAG), \
+        .param_count = (PARAM_COUNT), \
+        .binding_contract = PROTO_BINDING_SET_EMPTY() \
+    })
+
+#define PROTO_NATIVE_SIGNATURE_WITH_BINDINGS(RETURN_TAG, PARAM_COUNT, BINDING_SET) \
+    ((ProtoNativeSignature){ \
+        .return_type = (RETURN_TAG), \
+        .param_count = (PARAM_COUNT), \
+        .binding_contract = (BINDING_SET) \
+    })
+
+typedef struct {
     const char *name;
     ProtoNativeFn function;
     uint8_t min_arity;
     uint8_t max_arity;
+    ProtoNativeSignature signature;
 } ProtoNativeEntry;
 
 const ProtoNativeEntry *protonative_resolve(const char *name);
