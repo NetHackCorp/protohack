5c21d6d6576a94982e6bc08033b2a862ef3d1345
NetHackCorp <nethack.github495237@gmail.com>
2025-10-11 10:58:15 +0200

    feat: add PROTO_TYPE_PTR to ProtoTypeTag enum and update proto_type_tag_name function

--- include/protohack/types.h
+++ include/protohack/types.h
@@ -13,6 +13,7 @@ typedef enum {
     PROTO_TYPE_FLAG,
     PROTO_TYPE_TEXT,
     PROTO_TYPE_RAW,
+    PROTO_TYPE_PTR,
     PROTO_TYPE_NONE
 } ProtoTypeTag;
 
@@ -23,6 +24,7 @@ static inline const char *proto_type_tag_name(ProtoTypeTag tag) {
         case PROTO_TYPE_FLAG: return "flag";
         case PROTO_TYPE_TEXT: return "text";
         case PROTO_TYPE_RAW: return "raw";
+        case PROTO_TYPE_PTR: return "pointer";
         case PROTO_TYPE_NONE: return "none";
         default: return "unknown";
     }
